0ead5b08a8e26b8f8c88898dd567990d693bbbb4
chesco502 <97119222+chesco502@users.noreply.github.com>
2023-04-20 20:47:24 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -1,30 +1,54 @@
 #include <stdio.h>
-typedef struct reg {
-   int         conteudo;
-   struct reg *prox;
+typedef struct reg
+{
+  int conteudo;
+  struct reg *prox;
 } celula;
 typedef struct reg celula;
-    void print(struct reg a){
-    struct reg *ponteiro;
-    ponteiro = &a;
-    while(ponteiro!=NULL){
-        printf("%i\n",ponteiro->conteudo);
-        ponteiro=ponteiro->prox;
-        
+void
+print (struct reg a)
+{
+  struct reg *ponteiro;
+  ponteiro = &a;
+  while (ponteiro != NULL)
+    {
+      printf ("%i\n", ponteiro->conteudo);
+      ponteiro = ponteiro->prox;
+
     }
 }
 
-
-int main()
+void
+remover (struct reg a, struct reg n)
 {
-    celula a;
-    a.conteudo =1;
-    celula b;
-    b.conteudo=2;
-    a.prox=&b;
-    celula c;
-    c.conteudo=3;
-    b.prox=&c;
-    print(a);
-    return 0;
+    struct reg *ponteiro, *buffer,*N;
+    N=&n;
+    
+  while (ponteiro != NULL)
+    {
+      if (&ponteiro == &N)
+	{
+	  buffer->prox = ponteiro->prox;
+      printf("a");
+	}
+      buffer = ponteiro;
+      ponteiro = ponteiro->prox;
+
+    }
+}
+
+int
+main ()
+{
+  celula a;
+  a.conteudo = 1;
+  celula b;
+  b.conteudo = 2;
+  a.prox = &b;
+  celula c;
+  c.conteudo = 3;
+  b.prox = &c;
+  remover(a,b);
+  print (a);
+  return 0;
 }
